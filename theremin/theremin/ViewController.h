7538c656a5c33fc3b91d8aafc0bbff6e36a67d7e
McCall Bliss <missmccallkay@gmail.com>
2015-02-01 15:32:11 -0500

    gitignore file for xcode project

---
+++ theremin/theremin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  theremin
+//
+//  Created by McCall Bliss on 2/1/15.
+//  Copyright (c) 2015 tufts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
