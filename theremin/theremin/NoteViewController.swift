f6929c38418d8588b46a175bf0910529a55f09a6
trcolgrove <trcolgrove@gmail.com>
2015-02-07 23:14:28 -0500

    beginnin class implementation

--- theremin/theremin/NoteViewController.swift
+++ theremin/theremin/NoteViewController.swift
@@ -3,7 +3,22 @@
 //  theremin
 //
 //  Created by McCall Bliss on 2/5/15.
+//  Implemented by Thomas Colgrove
 //  Copyright (c) 2015 tufts. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+class NoteViewController {
+    var leftmost_note: Int = 0
+    var key: String = ""
+    var Grid: GridViewController
+    
+    init(){
+        leftmost_note = 60
+        key = "CM"
+        Grid = GridViewController()
+    }
+    
+}
