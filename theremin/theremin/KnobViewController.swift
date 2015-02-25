e1b0fe9fbedf98d070bf680085a2e6f6fb5da47f
McCall Bliss <missmccallkay@gmail.com>
2015-02-25 15:50:45 -0500

    added a knob view controller class & updated the knobs

--- theremin/theremin/KnobViewController.swift
+++ theremin/theremin/KnobViewController.swift
@@ -16,17 +16,53 @@ class KnobViewController: InstrumentViewController {
     var tremolo: Float = 0
     var gain: Float = 0
     
-    @IBOutlet var reverb_btn: UIButton?
-    @IBOutlet var chorus_btn: UIButton?
-    @IBOutlet var tremelo_btn: UIButton?
-    @IBOutlet var gain_btn: UIButton?
+    var reverb_knob: Knob!
+    
+    @IBOutlet var reverb_placeholder: UIView!
+    @IBOutlet var chorus_placeholder: UIView!
+    @IBOutlet var tremolo_placeholder: UIView!
+    @IBOutlet var gain_placeholder: UIView!
+    
+    @IBOutlet var reverb_value: UILabel!
+    @IBOutlet var chorus_value: UILabel!
+    @IBOutlet var tremolo_value: UILabel!
+    @IBOutlet var gain_value: UILabel!
+    
+    @IBOutlet var valueSlider: UISlider!
+    @IBOutlet var animateSwitch: UISwitch!
     
     override func viewDidLoad() {
-        println("Knob View Controller is loaded")
+        super.viewDidLoad()
+        
+        reverb_knob = Knob(frame: reverb_placeholder.bounds)
+        reverb_knob.addTarget(self, action: "knobValueChanged:", forControlEvents: .ValueChanged)
+        reverb_placeholder.addSubview(reverb_knob)
+        reverb_knob.value = valueSlider.value
+        updateLabel()
     }
     
-    @IBAction func openKnobControls (sender: UIButton) {
-        println("Opening knob controls")
+    func knobValueChanged(knob: Knob) {
+        valueSlider.value = knob.value
+        
+        updateLabel()
+    }
+    
+    @IBAction func sliderValueChanged(slider: UISlider) {
+        reverb_knob.value = slider.value
+        
+        updateLabel()
+    }
+    
+    @IBAction func randomButtonTouched(button: UIButton) {
+        let randomValue = Float(arc4random_uniform(101)) / 100.0
+        reverb_knob.setValue(randomValue, animated: animateSwitch.on)
+        valueSlider.setValue(randomValue, animated: animateSwitch.on)
+        
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        reverb_value.text = NSNumberFormatter.localizedStringFromNumber(reverb_knob.value, numberStyle: .NoStyle)
     }
     
     func updateEffect (effect: String, new_value: Float) {
