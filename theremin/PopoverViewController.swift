1d6887e2fa08adf5bc3eac528043b147d47a72ca
McCall Bliss <missmccallkay@gmail.com>
2015-04-27 18:29:03 -0400

    added pd message to waveform

--- theremin/PopoverViewController.swift
+++ theremin/PopoverViewController.swift
@@ -37,24 +37,25 @@ class PopoverViewController: UITableViewController, UITableViewDataSource, UITab
             if let parent_vc = parent as? InstrumentViewController {
                 parent_vc.grid.y_axis_string = self.options[indexPath.row]
                 parent_vc.y_effect.setTitle(self.options[indexPath.row], forState: UIControlState.Normal)
-                self.dismissViewController()
             }
         } else if popoverType == "wave" {
             if let parent_vc = parent as? SynthViewController {
                 
-                // send pd the new waveform
+                let wave_form = self.options[indexPath.row]
+                //let pd_wave_name = parent.getWaveName(wave_form)
+                
+                PdBase.sendList([wave_form], toReceiver: "waveform")
                 
                 if (segue == "wave_osc1") {
-                    parent_vc.wave_button_1.setTitle(self.options[indexPath.row], forState: UIControlState.Normal)
-                    println("osc 1")
+                    parent_vc.wave_button_1.setTitle(wave_form, forState: UIControlState.Normal)
                 } else if (segue == "wave_osc2") {
-                    parent_vc.wave_button_2.setTitle(self.options[indexPath.row], forState: UIControlState.Normal)
+                    parent_vc.wave_button_2.setTitle(wave_form, forState: UIControlState.Normal)
                 } else if (segue == "wave_osc3") {
-                    parent_vc.wave_button_3.setTitle(self.options[indexPath.row], forState: UIControlState.Normal)
+                    parent_vc.wave_button_3.setTitle(wave_form, forState: UIControlState.Normal)
                 }
-                self.dismissViewController()
             }
         }
+        self.dismissViewController()
     }
     
     func dismissViewController() {
