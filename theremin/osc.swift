c9ea1e9c6e6c4932e6bdd31855b3a0059e806135
trcolgrove <trcolgrove@gmail.com>
2015-04-27 22:09:52 -0400

    implemented dsp switch

--- theremin/osc.swift
+++
@@ -1,9 +0,0 @@
-//
-//  osc.swift
-//  theremin
-//
-//  Created by Thomas Colgrove on 4/21/15.
-//  Copyright (c) 2015 tufts. All rights reserved.
-//
-
-import Foundation
--- theremin/subsynth.swift
+++ theremin/subsynth.swift
@@ -1,5 +1,5 @@
 //
-//  subsynth_model.swift
+//  subsynth.swift
 //  theremin
 //
 //  Created by Thomas Colgrove on 4/21/15.
@@ -8,27 +8,16 @@
 
 import Foundation
 
-enum wave{
-    case SINE, TRIANGLE, SAWTOOTH, SQUARE, BRIGHT, IVORY, GLASS, CLAV, BASS_1, BASS_2, DEEP, METALLIC, ORGAN_1, ORGAN_2, BOW_1, BOW_2, BOW_3, STEEL, BRASS_1, BRASS_2, SAX, TRUMP, WOOD_1, WOOD_2
-}
-struct osc{
-    var waveform: wave
-    var phase_offset: CGFloat
-    var volume: CGFloat
-    var pitch_offset: Int
-    var on: Boolean
-}
+
 
 struct synth_setting{
-    var osc1: osc
-    var osc2: osc
-    var osc3: osc
+    var osc1: oscillator
+    var osc2: oscillator
+    var osc3: oscillator
 }
-/*
+
 class subsynth{
-    init(osc1, osc2, osc3){
+    init(){
         
     }
-    
 }
-*/
